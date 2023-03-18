b6e789c25750dec9c2c96a1269af15cf340ae511
Kuanysh Aptaizhanov <89320116+kuanysh-1998@users.noreply.github.com>
2023-03-18 21:07:53 +0300

    finished

--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -32,14 +32,18 @@ const FullPizza: React.FC = () => {
   }
   return (
     <div className="container">
-      <img src={pizza.imageUrl} />
-      <h2>{pizza.title}</h2>
-      <h4>{pizza.price} ₽</h4>
-      <Link to="/">
-        <button className="button button--outline button--add">
-          <span>Назад</span>
-        </button>
-      </Link>
+      <div className="fullpizza">
+        <img src={pizza.imageUrl} />
+        <div>
+          <h2>{pizza.title}</h2>
+          <h4>{pizza.price} ₽</h4>
+        </div>
+        <Link to="/">
+          <button className="button button--outline button--add">
+            <span>Назад</span>
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
